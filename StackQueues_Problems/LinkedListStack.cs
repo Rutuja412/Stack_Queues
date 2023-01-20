ab69084471250eef56ed24c4324917b813317ecb
Rutuja412 <rutujakadam4545@gmail.com>
2023-01-20 11:56:47 +0530

    [Rutuja]:Create Stack

---
+++ StackQueues_Problems/LinkedListStack.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+
+namespace StackQueues_Problems
+{
+    internal class LinkedListStack
+    {
+
+        public Node top;
+        public LinkedListStack()
+        {
+            this.top = null;
+        }
+        public void Push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine("{0} pushed to stack", value);
+        }
+        public void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.Write(temp.data + "->");
+                temp = temp.next;
+            }
+        }
+    }
+}
---
+++ StackQueues_Problems/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StackQueues_Problems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Stack & Queue Practice Problems");
+            LinkedListStack linkedListStack = new LinkedListStack();
+            linkedListStack.Push(70);
+            linkedListStack.Push(30);
+            linkedListStack.Push(56);
+            Console.Write("\n");
+            Console.WriteLine("Sequence of stack after push values are:  ");
+            Console.WriteLine("--------------------------------------------");
+            linkedListStack.Display();
+        }
+    }
+}
