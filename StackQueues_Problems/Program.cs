4173461178167eb224dc6a59c23466eed44c6a88
Rutuja412 <rutujakadam4545@gmail.com>
2023-01-20 13:22:22 +0530

    [Rutuja]:Ability to Dequeue

--- StackQueues_Problems/Program.cs
+++ StackQueues_Problems/Program.cs
@@ -7,25 +7,29 @@ namespace StackQueues_Problems
         static void Main(string[] args)
         {
             Console.WriteLine("Stack & Queue Practice Problems");
-            LinkedListStack linkedListStack = new LinkedListStack();
-            linkedListStack.Push(70);
-            linkedListStack.Push(30);
-            linkedListStack.Push(56);
-            Console.Write("\n");
-            Console.WriteLine("Sequence of stack after push values are:  ");
-            Console.WriteLine("--------------------------------------------");
-            linkedListStack.Display();
-            Console.WriteLine("--------------------------------------------");
-            linkedListStack.Peek();
-            Console.Write("\n");
-
-            Console.WriteLine("--------------------------------------------");
-            linkedListStack.Pop();
-            Console.Write("\n");
-
-            Console.WriteLine("--------------------------------------------");
-            linkedListStack.IsEmpty();
-            Console.Write("\n");
+            //LinkedListStack linkedListStack = new LinkedListStack();
+            //linkedListStack.Push(70);
+            //linkedListStack.Push(30);
+            //linkedListStack.Push(56);
+            //linkedListStack.Display();
+            //linkedListStack.Peek();
+            //linkedListStack.Pop();
+            LinkedListQueue linkedListQueue = new LinkedListQueue();
+            Console.WriteLine("----------------------------");
+            linkedListQueue.Enqueue(56);
+            linkedListQueue.Enqueue(30);
+            linkedListQueue.Enqueue(70);
+            Console.WriteLine("\n");
+            Console.WriteLine("Sequence of queue are: ");
+            Console.WriteLine("----------------------------");
+            linkedListQueue.Display();
+            Console.WriteLine("\n");
+            linkedListQueue.Dequeue();
+            linkedListQueue.Dequeue();
+            Console.WriteLine("----------------------------");
+            linkedListQueue.Display();
+            Console.WriteLine("\n");
+          
 
         }
     }
